b284868a6c0f290b1e0d210fba18a59f106d5d4f
Diego Sava Sastre <diegosegundo.sava@students.salle.url.edu>
2023-06-17 17:06:48 +0200

    signup and login working

--- app/src/main/java/com/example/giftgeek/Login.java
+++ app/src/main/java/com/example/giftgeek/Login.java
@@ -3,19 +3,32 @@ package com.example.giftgeek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    ImageView backButton;
+    Button logInButton;
+
+    EditText emailField;
+    EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ImageView backButton = (ImageView) findViewById(R.id.backButton_login);
+        backButton = (ImageView) findViewById(R.id.backButton_login);
+        logInButton = (Button) findViewById(R.id.loginButton_login);
+
+        emailField = (EditText) findViewById(R.id.emailField_login);
+        passwordField = (EditText) findViewById(R.id.passwordField_login);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +38,37 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                if(login(email, password)) {
+                    Toast.makeText(getApplicationContext(), "LogIn successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.setClass(Login.this, MainActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Wrong email or password.", Toast.LENGTH_SHORT).show();
+                    emailField.setText("");
+                    passwordField.setText("");
+                }
+            }
+        });
+    }
+
+    public boolean login(String email, String password) {
+        boolean success;
+        SharedPreferences sharedPreferences = getSharedPreferences("UserCredentials", MODE_PRIVATE);
+
+        String pass = sharedPreferences.getString(email, "not found");
+
+        if (pass.equals("not found")) {
+            success = false;
+        }else success = password.equals(pass);
+
+        return success;
     }
 }
--- app/src/main/java/com/example/giftgeek/SignUp.java
+++ app/src/main/java/com/example/giftgeek/SignUp.java
@@ -3,18 +3,33 @@ package com.example.giftgeek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
+
+    ImageView backButton;
+    Button signUpButton;
+
+    EditText emailField;
+    EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        ImageView backButton = (ImageView) findViewById(R.id.backButton_signUp);
+        backButton = (ImageView) findViewById(R.id.backButton_signUp);
+        signUpButton = (Button) findViewById(R.id.signupButton_signup);
+
+        emailField = (EditText) findViewById(R.id.emailField_signUp);
+        passwordField = (EditText) findViewById(R.id.passwordField_signUp);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +39,45 @@ public class SignUp extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                if(signUp(email, password)){
+                    Toast.makeText(getApplicationContext(), "Sign Up successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent();
+                    intent.setClass(SignUp.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Email already exists", Toast.LENGTH_SHORT).show();
+                    emailField.setText("");
+                    passwordField.setText("");
+                }
+            }
+        });
+    }
+
+    public boolean signUp(String email, String password) {
+        boolean success;
+        SharedPreferences sharedPreferences = getSharedPreferences("UserCredentials", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String available = sharedPreferences.getString(email, "not found");
+
+        if(available.equals("not found")) {
+
+            editor.putString(email, password);
+            editor.commit();
+
+            success = true;
+        } else {
+            success = false;
+        }
+
+
+        return success;
     }
 }
