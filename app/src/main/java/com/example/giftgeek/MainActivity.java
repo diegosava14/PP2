2a2cea701fa392fea18ab1126d3a2841495ae06b
Diego Sava Sastre <diegosegundo.sava@students.salle.url.edu>
2023-06-18 12:22:09 +0200

    fragment structure setup

--- app/src/main/java/com/example/giftgeek/MainActivity.java
+++ app/src/main/java/com/example/giftgeek/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.giftgeek;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.giftgeek.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new WishListFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.search:
+                    replaceFragment(new SearchFragment());
+                    break;
+                case R.id.home:
+                    replaceFragment(new WishListFragment());
+                    break;
+
+                case R.id.user:
+                    replaceFragment(new UserFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
     }
 }
