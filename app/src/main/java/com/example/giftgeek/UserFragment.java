135d3a6d854b16316b5156b049a37ccf60a95302
Diego Sava Sastre <diegosegundo.sava@students.salle.url.edu>
2023-06-18 14:24:31 +0200

    profile design implemented

--- app/src/main/java/com/example/giftgeek/UserFragment.java
+++ app/src/main/java/com/example/giftgeek/UserFragment.java
@@ -1,6 +1,7 @@
 package com.example.giftgeek;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,10 +10,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.SharedPreferences;
 
 public class UserFragment extends Fragment {
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences sharedPreferences2;
+
     private String loggedInUser;
+    private TextView email;
+    private TextView username;
+
+    private Button logoutButton;
+    private Button statsButton;
+    private ImageView editProfileButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +37,19 @@ public class UserFragment extends Fragment {
         if (arguments != null) {
             loggedInUser = arguments.getString("loggedInUser");
         }
+
+        sharedPreferences = getActivity().getSharedPreferences("UserCredentials", getActivity().MODE_PRIVATE);
+        sharedPreferences2 = getActivity().getSharedPreferences("Usernames", getActivity().MODE_PRIVATE);
+
+        email = view.findViewById(R.id.email_textview);
+        username = view.findViewById(R.id.username_textview);
+
+        logoutButton = view.findViewById(R.id.logoutButton);
+        statsButton = view.findViewById(R.id.statsButton);
+        editProfileButton = view.findViewById(R.id.editButton);
+
+        email.setText(loggedInUser);
+        username.setText(sharedPreferences2.getString(loggedInUser, "Username"));
 
         return view;
     }
